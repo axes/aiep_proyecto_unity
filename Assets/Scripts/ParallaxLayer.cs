d0b431a54b99a90ef28cd853c31beeb0a1680c2c
Axes <gino.villablanca@gmail.com>
2025-07-14 11:04:17 -0400

    Ajustes de backgrounds para MenuInicial e Historia

---
+++ Assets/Scripts/ParallaxLayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float parallaxFactor = 0.5f;
+    private Vector3 lastCameraPos;
+
+    void Start()
+    {
+        lastCameraPos = cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = cameraTransform.position - lastCameraPos;
+        transform.position += new Vector3(delta.x * parallaxFactor, delta.y * parallaxFactor, 0);
+        lastCameraPos = cameraTransform.position;
+    }
+    
+}
